e576442de91ab249ab2bd90af02f879775c7b537
Your Name <alan.ibarra209@gmail.com>
2018-09-09 17:15:44 -0600

    step1 of wizard connected to redux

--- src/components/PropertyAddWizard/Step1.js
+++ src/components/PropertyAddWizard/Step1.js
@@ -1,34 +1,73 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
+import { setPropertyAddress } from '../../redux/reducer';
 class Step1 extends Component {
+    constructor() {
+        super();
+        this.state = {
+            propertyStreet: '',
+            propertyCity: '',
+            propertyState: '',
+            propertyZipcode: ''
+        }
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.updateWizard = this.updateWizard.bind(this);
+    }
     componentDidMount() {
         this.props.onStep('step1');
         this.props.updateStep('step1');
+        let { propertyStreet, propertyCity, propertyState, propertyZipcode } = this.props;
+        this.setState({
+            propertyStreet, propertyCity, propertyState, propertyZipcode
+        })
     }
+
+    handleInputChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    updateWizard() {
+        let { propertyStreet, propertyCity, propertyState, propertyZipcode } = this.state;
+        this.props.setPropertyAddress(propertyStreet, propertyCity, propertyState, propertyZipcode);
+        this.props.updateStep('step2')
+    }
+
     render() {
         return (
             <div className="step1">
                 <h4 className="wizard-step-title">Property Address</h4>
                 <div className="address-information">
                     <div className="input-group">
+                        <h6>Street</h6>
+                        <input value={this.state.propertyStreet} onChange={this.handleInputChange} name="propertyStreet" type="text" />
+                    </div>
+                    <div className="input-group">
                         <h6>City</h6>
-                        <input type="text" />
+                        <input value={this.state.propertyCity} onChange={this.handleInputChange} name="propertyCity" type="text" />
                     </div>
                     <div className="input-group">
                         <h6>State</h6>
-                        <input type="text" />
+                        <input value={this.state.propertyState} onChange={this.handleInputChange} name="propertyState" type="text" />
                     </div>
                     <div className="input-group">
                         <h6>Zipcode</h6>
-                        <input type="text" />
+                        <input value={this.state.propertyZipcode} onChange={this.handleInputChange} name="propertyZipcode" type="text" />
                     </div>
+
                 </div>
                 <div className="wizard-controls">
-                    <button onClick={() => this.props.updateStep('step2')}>Next Step</button>
+                    <button onClick={this.updateWizard}>Next Step</button>
                 </div>
             </div>
         )
     }
 }
 
-export default Step1;
+function mapStateToProps(state) {
+    let { propertyStreet, propertyCity, propertyState, propertyZipcode } = state;
+    return {
+        propertyStreet, propertyCity, propertyState, propertyZipcode
+    }
+}
+
+export default connect(mapStateToProps, { setPropertyAddress })(Step1);
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,4 +1,5 @@
-const intialState = {
+let initialState = {
+    propertyStreet: '',
     propertyCity: '',
     propertyState: '',
     propertyZipcode: '',
@@ -11,9 +12,26 @@ const intialState = {
     propertImg: ''
 }
 
-export default function(state = intialState, action){
-    switch(action.type){
+const SET_PROPERTY_ADDRESS = 'PROPERTY_ADDRESS';
+
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        case SET_PROPERTY_ADDRESS:
+            let {propertyStreet,  propertyCity, propertyState, propertyZipcode } = action.payload;
+            return Object.assign({}, state, { propertyStreet, propertyCity, propertyState, propertyZipcode })
         default:
             return state;
     }
 }
+
+export function setPropertyAddress(propertyStreet, propertyCity, propertyState, propertyZipcode) {
+    return {
+        type: SET_PROPERTY_ADDRESS,
+        payload: {
+            propertyStreet, propertyCity, propertyState, propertyZipcode
+        }
+    }
+}
+
+
