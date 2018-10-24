15c638cde13442dc4855636903fa2894ef753a74
Your Name <alan.ibarra209@gmail.com>
2018-10-24 13:44:12 -0700

    Stipe setup on the front end

--- src/components/Payment/Payment.js
+++ src/components/Payment/Payment.js
@@ -3,11 +3,14 @@ import PaymentForm from './PaymentForm';
 import { Elements, StripeProvider } from 'react-stripe-elements';
 class Payment extends Component {
 
-    handleChange = (e) =>{
+    handleChange = (e) => {
         // console.log(e.target.value)
         console.log(e);
     }
 
+    handleSubmit = () => {
+        console.log('test');
+    }
 
     render() {
         return (
@@ -25,31 +28,16 @@ class Payment extends Component {
                 <header className="header header-payment">
                     <StripeProvider apiKey="pk_test_W69mdTmwiZ7lJv2OFwiwXUX1">
                         <div className="payment">
-                            <div className="payment-description">
+                            {/* <div className="payment-description">
                                 <h1>Houser's Payment System</h1>
                                 <p>
                                     Welcome tenants to process a payment fill the information to the right.  If all
                                     the information is correct your payment will be processed and your landlord will be
                                     notified.
                                 </p>
-                                <button>Submit Payment</button>
-                            </div>
-                            {/* <div className="tenant-info">
-                                <div className="input-group">
-                                    <label htmlFor="">Name</label>
-                                    <input type="text" />
-                                </div>
-                                <div className="input-group">
-                                    <label htmlFor="">Last 4 Digits of SSN</label>
-                                    <input type="text" />
-                                </div>
-                                <div className="input-group">
-                                    <label htmlFor="">Amount</label>
-                                    <input type="text" />
-                                </div>
                             </div> */}
                             <Elements>
-                                <PaymentForm handleChange={this.handleChange} />
+                                <PaymentForm handleSubmit={this.handleSubmit} handleChange={this.handleChange} />
                             </Elements>
                         </div>
                     </StripeProvider>
--- src/components/Payment/PaymentForm.js
+++ src/components/Payment/PaymentForm.js
@@ -4,34 +4,68 @@ import { CardNumberElement, CardCVCElement, CardExpiryElement, injectStripe } fr
 class PaymentForm extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            name: '',
+            ssn: 0,
+            amount: 0
+        }
     }
-    handleSubmit = async (ev) => {
-        let {token} = await this.props.stripe.createToken({name: "Name"});
-        console.log(token)
+    handleSubmit = async () => {
+        let { token } = await this.props.stripe.createToken({ name: "Name" });
+    }
+    handleChange = e => {
+        let { value } = e.target;
+        if (e.target.name === 'ssn' || e.target.name === 'amount') {
+            value = parseInt(e.target.value);
+        }
+        this.setState({ [e.target.name]: value })
     }
     render() {
         return (
-            <div className="payment-form">
-                <form onSubmit={this.handleSubmit}>
+            <div className="payment-form" onSubmit={this.handleSubmit}>
+                <div className="payment-description">
+                    <h1>Houser's Payment System</h1>
+                    <p>
+                        Welcome tenants to process a payment fill the information to the right.  If all
+                        the information is correct your payment will be processed and your landlord will be
+                        notified.
+                                </p>
+                    <button onClick={this.handleSubmit}>Sumbit Payment</button>
+                </div>
+                <div className="tenant-info">
+                    <div className="input-group">
+                        <label htmlFor="">Name</label>
+                        <input name='name' onChange={this.handleChange} type="text" />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="">Last 4 Digits of SSN</label>
+                        <input name='ssn' onChange={this.handleChange} type="text" />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="">Amount</label>
+                        <input name='amount' onChange={this.handleChange} type="text" />
+                    </div>
+                </div>
+                <div className="payment-info">
                     <div className="input-group">
                         Card number
                    <CardNumberElement
-                    onChange={this.props.handleChange}
+                            onChange={this.props.handleChange}
                         />
                     </div>
                     <div className="input-group">
                         Expiration Date
                     <CardExpiryElement
-                        onChange={this.props.handleChange}
-                    />
+                            onChange={this.props.handleChange}
+                        />
                     </div>
                     <div className="input-group">
                         CVC
                     <CardCVCElement
-                        onChange={this.props.handleChange}
-                    />
+                            onChange={this.props.handleChange}
+                        />
                     </div>
-                </form>
+                </div>
             </div>
         )
     }
